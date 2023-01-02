4134a084246b2255674101e7ab6508e40f9d1a5f
Ahmed Shoura <88495441+shoura279@users.noreply.github.com>
2023-01-02 04:12:59 +0200

    Add files via upload

---
+++ templet.h
@@ -0,0 +1,22 @@
+//  " All You Need is Love <3 "
+
+using namespace std;
+#include          <bits/stdc++.h>
+#define   AllYouNeedisLove  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define   yes     cout<<"YES \n"
+#define   no      cout<<"NO \n"
+#define   tc      int t;cin>>t;while(t--)
+#define   ll      long long
+#define   ull     unsigned long long
+#define   dd      double
+#define   el      "\n"
+#define   root(n) (-1+sqrt(1-4*2*(-n)))/2
+ll const  N      = 2e5;
+ll mod=1000000007;
+void solve() {
+    
+}
+int32_t main() {
+    AllYouNeedisLove
+    tc solve(),cout<<el;
+}
